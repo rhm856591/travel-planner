16aebf700c21f09f43827893d30a920805541967
Rahamtullah Sheikh <sheikh856591@gmail.com>
2023-12-23 00:47:32 +0530

    conflict resolution

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import Image from 'next/image'
-import BG from "../assets/images/bg2.jpeg"
 import "../styles/style.css"
 
 
 
 const Hero = () => {
     return (
-        <section className="home" id="home">
-
-
+        <section className="home" id="home" style={{ backgroundImage: `url("https://res.cloudinary.com/dvsp7dq94/image/upload/v1703271098/i28rywqps5xdir63mxv7.jpg")` }}>
 
             <div className="content">
                 <h3>adventure is worthwhile</h3>
