480907fefe73e9e0d17d36c29bfd94c9dd30b1b8
Rahamtullah Sheikh <sheikh856591@gmail.com>
2023-12-22 23:04:19 +0530

    adding footer and contact in component

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div><h1 className="credit"> Copyright &copy;2021. All Rights Reserved. </h1></div>
+  )
+}
+
+export default footer
