49f385b1d2fd51fd4e15dd11f6c25ca0c9ae14db
Aditya Kumar Choudhary <adityakrchy101@gmail.com>
2023-12-22 23:12:54 +0530

    fixed contact form

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,42 +1,42 @@
 import React from 'react'
 
 const contact = () => {
-  return (
-    <section className="contact" id="contact">
+    return (
+        <section className="contact" id="contact">
 
-        <h1 className="heading">
-            <span>c</span>
-            <span>o</span>
-            <span>n</span>
-            <span>t</span>
-            <span>a</span>
-            <span>c</span>
-            <span>t</span>
-        </h1>
+            <h1 className="heading">
+                <span>c</span>
+                <span>o</span>
+                <span>n</span>
+                <span>t</span>
+                <span>a</span>
+                <span>c</span>
+                <span>t</span>
+            </h1>
 
-        <div className="row">
+            <div className="row">
 
-            <div className="image">
-                <img src="images/contact-img.svg" alt="">
+                <div className="image">
+                    <img src="images/contact-img.svg" alt="" />
+                </div>
+
+                <form action="">
+                    <div className="inputBox">
+                        <input type="text" name="name" placeholder="name" />
+                        <input type="email" name="email" placeholder="email" />
+                    </div>
+                    <div className="inputBox">
+                        <input type="number" name="phone" placeholder="number" />
+                        <input type="text" name="subject" placeholder="subject" />
+                    </div>
+                    <textarea placeholder="message" name="message" id="" cols={30} rows={10}></textarea>
+                    <input type="submit" className="btn" value="send message" />
+                </form>
+
             </div>
 
-            <form action="">
-                <div className="inputBox">
-                    <input type="text" name="name" placeholder="name">
-                    <input type="email" name="email" placeholder="email">
-                </div>
-                <div className="inputBox">
-                    <input type="number" name="phone" placeholder="number">
-                    <input type="text" name="subject" placeholder="subject">
-                </div>
-                <textarea placeholder="message" name="message" id="" cols="30" rows="10"></textarea>
-                <input type="submit" className="btn" value="send message">
-            </form>
-
-        </div>
-
-    </section>
-  )
+        </section>
+    )
 }
 
 export default contact
