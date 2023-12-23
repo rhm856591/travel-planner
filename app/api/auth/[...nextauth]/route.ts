20e5b0b7c4ecfccbb3fca72824ef93165925b54d
Aditya Kumar Choudhary <adityakrchy101@gmail.com>
2023-12-23 13:29:32 +0530

    added nextauth route

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions: NextAuthOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+                if (user) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        })
+    ],
+}
+
+export default NextAuth(authOptions)
