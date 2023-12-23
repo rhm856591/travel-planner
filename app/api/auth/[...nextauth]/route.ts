7da20f27dba85c42efade17367e9b3cf811a3710
Aditya Kumar Choudhary <adityakrchy101@gmail.com>
2023-12-23 15:30:54 +0530

    added authentication module and linked it with sign in page and setup protected route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,15 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import nextAuth from "next-auth";
 import NextAuth, { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google"
+import db from "@/prisma/db";
+import bcrypt from "bcrypt";
+
 
 export const authOptions: NextAuthOptions = {
-    // Configure one or more authentication providers
+    adapter: PrismaAdapter(db),
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -12,21 +18,35 @@ export const authOptions: NextAuthOptions = {
         CredentialsProvider({
             name: "Credentials",
             credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                email: { label: "Email", type: "text", placeholder: "someone@example.com" },
                 password: { label: "Password", type: "password" }
             },
-            async authorize(credentials, req) {
-                // Add logic here to look up the user from the credentials supplied
-                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+            async authorize(credentials) {
+                // console.log("credentials", credentials)
 
-                if (user) {
-                    return user
-                } else {
-                    return null
+                const user = await db.user.findFirst({
+                    where: {
+                        email: credentials?.email
+                    }
+                })
+
+                if (!user) {
+                    throw new Error("No user found")
                 }
+                const validPassword = await bcrypt.compare(credentials?.password as string, user?.password as string)
+
+                if (!validPassword) {
+                    throw new Error("Invalid password")
+                }
+                
+                return user
             }
-        })
+        },)
     ],
+    pages: {
+        signIn: "/auth/signin",
+    }
 }
 
-export default NextAuth(authOptions)
+const handler = nextAuth(authOptions);
+export { handler as GET, handler as POST }
